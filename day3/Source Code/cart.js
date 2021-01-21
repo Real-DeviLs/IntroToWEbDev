9d812405308ecfaf61c91dea77b2a523752cfd3f
Gupta-Anubhav12 <anubhavgupta2260@gmail.com>
2021-01-22 02:28:23 +0530

    Hey

---
+++ day3/Source Code/cart.js
@@ -0,0 +1,107 @@
+var cart = [];
+
+
+window.onload = function loaded(){
+    if (localStorage.cart) {
+        cart = JSON.parse(localStorage.cart);
+        showCart();
+    }
+
+}
+
+
+
+function addToCart(id, name, price) {
+    console.log("cart");
+
+    var price = price;
+    var name = name;
+    var id = id;
+    var qty = 1;
+
+    // update qty if product is already present
+    for (var i in cart) {
+        if (cart[i].Id == id) {
+            cart[i].Qty += 1;
+            saveCart();
+            showCart();
+
+            return;
+        }
+    }
+    // create JavaScript Object
+    var item = {
+        Id: id,
+        Product: name,
+        Price: price,
+        Qty: qty
+    };
+    cart.push(item);
+    console.log(cart);
+    saveCart();
+    showCart();
+}
+
+
+
+function subtractFromCart(i) {
+    // update qty if product is already present
+        
+            cart[i].Qty += -1;
+            console.log(i);
+            if(cart[i].Qty==0)
+            deleteItem(i);
+
+            showCart();
+            saveCart();
+            return;
+    
+}
+
+function incrementCart(i)
+{
+    cart[i].Qty += 1;
+    showCart();
+    saveCart();
+    return;
+    
+
+}
+
+
+function deleteItem(index) {
+    cart.splice(index, 1); // delete item at index
+    showCart();
+    saveCart();
+}
+
+function saveCart() {
+    if (window.localStorage) {
+        localStorage.cart = JSON.stringify(cart);
+    }
+}
+
+function showCart() {
+    if (cart.length == 0) {
+        document.getElementById("cart").style.visibility='hidden';
+        document.getElementById("checkoutButton").style.visibility='hidden';
+        return;
+    }
+
+    
+    document.getElementById("cart").style.visibility='visible';
+    document.getElementById("checkoutButton").style.visibility='visible';
+
+    document.getElementById("cartBody").innerHTML="";
+    for (var i in cart) {
+        var item = cart[i];
+
+        document.getElementById("cartBody").innerHTML += 
+             "<tr><td>" + item.Product + "</td><td>" +
+            item.Price + "</td><td>" + item.Qty + "</td><td>" +
+            item.Qty * item.Price + "</td><td>" +
+            "<button onclick='deleteItem(" + i + ")'>Delete</button> </td><td>"+
+            "<button onclick='incrementCart(" + i + ")'>+</button> </td><td>"+
+            "<button onclick='subtractFromCart("+ i +")'>-</button></td></tr>";
+    }
+}
---
+++ day3/Source Code/email.js
@@ -0,0 +1,65 @@
+var final = ""
+var clientEmail = "ludhianatechcom@gmail.com"
+var clientFinal = ""
+var orderPlaced = "Order Placed"
+var newOrder = "New Order"
+
+
+
+function myFunction() {
+
+    console.log("hello");
+    var first_name = String(document.getElementById("first-name").value);
+    var last_name = String(document.getElementById("last-name").value);
+    var email = String(document.getElementById("email").value);
+    var phone = String(document.getElementById("phone").value);
+    var address = String(document.getElementById("address").value);
+    var state = String(document.getElementById("state").value);
+    var city = String(document.getElementById("city").value);
+    var pincode = String(document.getElementById("pincode").value);
+
+    var order = JSON.parse(localStorage.getItem("cart"));
+
+
+    console.log(order);
+    var product = "";
+    var productIncart = "";
+    var total = 0;
+
+
+    for (let i = 0; i < order.length; i += 1) {
+        total = total + (order[i].Price * order[i].Qty);
+        product = "<h2>Name : " + String(order[i].Product) + "</h2><h2>Quantity : " + String(order[i].Qty) + "</h2><h2>Price : " + String(order[i].Price) + "</h2>";
+        productIncart = productIncart + product + "<br></br>";
+    }
+    productIncart = productIncart + "<h2>Total : " + String(total) + "</h2>"
+    final = "<html><h2>Name : " + first_name + " " + last_name + "</h2><h2>Email : " + email + "</h2><h2>Phone : " + phone + "</h2><h2>Address : " + address + "</h2><h2>State : " + state + "</h2><h2>City : " + city + "</h2><h2>Pin Code : " + pincode + "</h2><br></br><h1>Order Details</h1>" + productIncart + "</html>"
+    clientFinal = "<html><h1>Your Order has been placed</h1><h1>Order Details</h1>" + productIncart + "</html>";
+
+
+    
+    sendEmail(clientEmail, final, newOrder);
+    sendEmail(email, final, orderPlaced);
+    localStorage.clear();
+}
+
+
+
+function sendEmail(rec,content, subject) {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "ludhianatechcom@gmail.com",
+        Password: "fvmozldcqazvgtim",
+        To: rec,
+        From: "ludhianatechcom@gmail.com",
+        Subject: subject,
+        Body: content,
+    }).then(
+        alert("mail sent successfully")
+    );
+}
+
+
+
+
+
